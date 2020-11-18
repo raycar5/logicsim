d38e66abbc7c47fd4f1d204ecdd073bb24e3862d
raycar5 <raycar5@users.noreply.github.com>
2020-11-18 14:02:57 +0100

    unified state bits

--- src/state.rs
+++ src/state.rs
@@ -3,7 +3,6 @@ use pretty_hex::*;
 #[derive(Hash)]
 pub struct State {
     states: Vec<u64>,
-    updated: Vec<u64>,
 }
 fn word_mask(index: usize) -> (usize, u64) {
     let word = index / 64;
@@ -12,17 +11,15 @@ fn word_mask(index: usize) -> (usize, u64) {
 }
 impl State {
     pub fn new(size: usize) -> State {
-        let len = size / 64;
+        let len = size * 2 / 64;
         let mut states = Vec::new();
-        let mut updated = Vec::new();
         states.reserve(len);
-        updated.reserve(len);
-        State { states, updated }
+        State { states }
     }
     #[inline(always)]
-    fn get_from_bit_vec(vec: &Vec<u64>, index: usize) -> bool {
-        let (word_index, mask) = word_mask(index);
-        let word = vec.get(word_index);
+    fn get_from_bit_vec(&self, real_index: usize) -> bool {
+        let (word_index, mask) = word_mask(real_index);
+        let word = self.states.get(word_index);
         if let Some(word) = word {
             word & mask != 0
         } else {
@@ -36,30 +33,27 @@ impl State {
         if index == ON {
             return true;
         }
-        Self::get_from_bit_vec(&self.states, index)
+        self.get_from_bit_vec(index * 2)
     }
     pub fn get_updated(&self, index: usize) -> bool {
         if index == OFF || index == ON {
             return true;
         }
-        Self::get_from_bit_vec(&self.updated, index)
+        self.get_from_bit_vec(index * 2 + 1)
     }
     pub fn set(&mut self, index: usize, value: bool) {
-        let (word_index, mask) = word_mask(index);
+        let (word_index, mask) = word_mask(index * 2);
+        let updated_bit_mask = mask << 1;
 
         let len = self.states.len();
         let diff = word_index as i64 + 1 - len as i64;
         if diff > 0 {
             self.states.reserve(diff as usize);
-            self.updated.reserve(diff as usize);
             self.states.extend((0..diff).step_by(1).map(|_| 0u64));
-            self.updated.extend((0..diff).step_by(1).map(|_| 0u64));
         }
 
-        let updated = &mut self.updated[word_index];
-        *updated = *updated | mask;
-
         let state = &mut self.states[word_index];
+        *state = *state | updated_bit_mask;
         if value {
             *state = *state | mask;
         } else {
@@ -67,12 +61,16 @@ impl State {
         }
     }
     pub fn flip(&mut self, index: usize) {
-        let (word_index, mask) = word_mask(index);
-        let updated = &mut self.updated[word_index];
-        *updated = *updated ^ mask;
+        let (word_index, mask) = word_mask(index * 2);
+        let state = &mut self.states[word_index];
+        *state = *state ^ mask;
     }
     pub fn tick(&mut self) {
-        self.updated.iter_mut().for_each(|x| *x = 0);
+        // clear all odd bits;
+        for state in &mut self.states {
+            let mask: u64 = 0x5555555555555555; // pattern 010101 etc..
+            *state = *state & mask;
+        }
     }
     pub fn dump(&self) {
         let slice = unsafe {
@@ -82,5 +80,50 @@ impl State {
             )
         };
         println!("{}", pretty_hex(&slice));
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_set() {
+        for i in 0..100 {
+            let mut state = State::new(1);
+            assert_eq!(state.get_state(i), false);
+            assert_eq!(state.get_updated(i), false);
+
+            state.set(i, true);
+
+            assert_eq!(state.get_state(i), true);
+            assert_eq!(state.get_updated(i), true);
+
+            state.set(i, false);
+
+            assert_eq!(state.get_state(i), false);
+            assert_eq!(state.get_updated(i), true);
+        }
+    }
+
+    #[test]
+    fn test_tick() {
+        let mut state = State::new(1);
+        for i in 0..100 {
+            assert_eq!(state.get_state(i), false, "index: {}", i);
+            assert_eq!(state.get_updated(i), false, "index: {}", i);
+
+            state.set(i, true);
+
+            assert_eq!(state.get_state(i), true, "index: {}", i);
+            assert_eq!(state.get_updated(i), true, "index: {}", i);
+
+            state.dump();
+            state.tick();
+            state.dump();
+
+            assert_eq!(state.get_state(i), true, "index: {}", i);
+            assert_eq!(state.get_updated(i), false, "index: {}", i);
+        }
     }
 }
