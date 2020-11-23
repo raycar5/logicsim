87d6143c64ce6de32a9847903deff7c48d139793
raycar5 <none@ofyourbusiness.com>
2020-11-23 12:31:26 +0100

    17% less instructions with a bit more unsafe

--- src/slab.rs
+++ src/slab.rs
@@ -1,18 +1,22 @@
+use indexmap::IndexSet;
+use std::mem::MaybeUninit;
 pub struct Slab<T: Sized> {
-    data: Vec<Option<T>>,
-    spaces: Vec<usize>,
+    data: Vec<MaybeUninit<T>>,
+    empty_spaces: IndexSet<usize>,
 }
 pub struct SlabIter<'a, T> {
-    iter: std::iter::Enumerate<std::slice::Iter<'a, Option<T>>>,
+    iter: std::iter::Enumerate<std::slice::Iter<'a, MaybeUninit<T>>>,
+    empty_spaces: &'a IndexSet<usize>,
 }
 impl<'a, T> Iterator for SlabIter<'a, T> {
     type Item = (usize, &'a T);
     fn next(&mut self) -> Option<Self::Item> {
         loop {
             let (i, item) = self.iter.next()?;
-            if let Some(item) = item {
-                return Some((i, item));
+            if self.empty_spaces.contains(&i) {
+                continue;
             }
+            unsafe { return Some((i, item.assume_init_ref())) };
         }
     }
 }
@@ -20,38 +24,55 @@ impl<T: Sized> Slab<T> {
     pub fn new() -> Self {
         Self {
             data: Vec::new(),
-            spaces: Vec::new(),
+            empty_spaces: IndexSet::new(),
         }
     }
     pub fn insert(&mut self, item: T) -> usize {
-        if let Some(space) = self.spaces.pop() {
-            self.data[space] = Some(item);
+        if let Some(space) = self.empty_spaces.pop() {
+            self.data[space] = MaybeUninit::new(item);
             space
         } else {
             let space = self.data.len();
-            self.data.push(Some(item));
+            self.data.push(MaybeUninit::new(item));
             space
         }
     }
     pub fn get_mut(&mut self, space: usize) -> Option<&mut T> {
-        self.data
-            .get_mut(space)
-            .and_then(|x| if let Some(item) = x { Some(item) } else { None })
+        if let Some(item) = self.data.get_mut(space) {
+            if self.empty_spaces.contains(&space) {
+                return None;
+            }
+            unsafe { return Some(item.assume_init_mut()) };
+        }
+        None
+    }
+    pub unsafe fn get_very_unsafely(&self, space: usize) -> &T {
+        debug_assert!(space < self.data.len());
+        debug_assert!(!self.empty_spaces.contains(&space));
+        self.data.get_unchecked(space).assume_init_ref()
     }
     pub fn get(&self, space: usize) -> Option<&T> {
-        self.data
-            .get(space)
-            .and_then(|x| if let Some(item) = x { Some(item) } else { None })
+        if let Some(item) = self.data.get(space) {
+            if self.empty_spaces.contains(&space) {
+                return None;
+            }
+            unsafe { return Some(item.assume_init_ref()) };
+        }
+        None
     }
     pub fn remove(&mut self, space: usize) -> Option<T> {
         if let Some(position) = self.data.get_mut(space) {
-            self.spaces.push(space);
-            return position.take();
+            if self.empty_spaces.contains(&space) {
+                return None;
+            }
+            self.empty_spaces.insert(space);
+            let item = std::mem::replace(position, MaybeUninit::uninit());
+            unsafe { return Some(item.assume_init()) };
         }
         None
     }
     pub fn len(&self) -> usize {
-        self.data.len() - self.spaces.len()
+        self.data.len() - self.empty_spaces.len()
     }
     pub fn total_len(&self) -> usize {
         self.data.len()
@@ -59,6 +80,7 @@ impl<T: Sized> Slab<T> {
     pub fn iter<'a>(&'a self) -> SlabIter<'a, T> {
         SlabIter {
             iter: self.data.iter().enumerate(),
+            empty_spaces: &self.empty_spaces,
         }
     }
 }
