c7a263aad559df911aee99e9bdb793af73d16630
raycar5 <raycar5@users.noreply.github.com>
2020-11-30 23:17:23 +0100

    refactor out dev dependencies

--- src/lib.rs
+++ src/lib.rs
@@ -220,7 +220,7 @@ needs some love.
 #[macro_use]
 pub mod graph;
 pub mod data_structures;
-extern crate concat_idents;
+pub extern crate concat_idents;
 pub mod circuits;
 pub use circuits::*;
 pub use graph::*;
